a278e6c636fcc25d444f7d9540154227bd511059
rakk <Robert.Kapala@gmail.com>
2016-11-01 13:25:05 +0100

    Creating initial repo

---
+++ src/main/java/org/rakk/Migration.java
@@ -0,0 +1,48 @@
+package org.rakk;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+@Mojo(name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+public class Migration extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project.build.directory}", property = "outputDir", required = true)
+    private File outputDirectory;
+
+    public void execute()
+            throws MojoExecutionException {
+        File f = outputDirectory;
+
+        if (!f.exists()) {
+            f.mkdirs();
+        }
+
+        File touch = new File(f, "touch.txt");
+
+        FileWriter w = null;
+        try {
+            w = new FileWriter(touch);
+
+            w.write("touch.txt");
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + touch, e);
+        } finally {
+            if (w != null) {
+                try {
+                    w.close();
+                } catch (IOException e) {
+                    // ignore
+                }
+            }
+        }
+    }
+}
